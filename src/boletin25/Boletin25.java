457ddee6f9f15817e0bbe5bb544f4ff452f161d5
cromerofajar <cromerofajar@nautilus7.danielcastelao.org>
2019-04-01 12:31:10 +0200

    Boletin25

---
+++ src/boletin25/Boletin25.java
@@ -0,0 +1,15 @@
+package boletin25;
+
+/**
+ *
+ * @author cromerofajar
+ */
+public class Boletin25 {
+
+    public static void main(String[] args) {
+
+        Ventana2 obxVen2=new Ventana2();
+        obxVen2.setVisible(true);
+    }
+    
+}
